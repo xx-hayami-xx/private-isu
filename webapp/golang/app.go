cd95b96a31771e5b9b61721873224e5c8605e2c2
volgakurvar <kurvan1112@gmail.com>
2023-10-22 00:30:59 +0900

    pprof

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -22,6 +22,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
+	import _ "net/http/pprof"
 )
 
 var (
@@ -792,6 +793,11 @@ func postAdminBanned(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	runtime.SetBlockProfileRate(1)
+    runtime.SetMutexProfileFraction(1)
+    go func() {
+        log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
+    }()
 	host := os.Getenv("ISUCONP_DB_HOST")
 	if host == "" {
 		host = "localhost"
